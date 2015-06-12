ca25076129bf4b81721ad8013e95845721459c99
Yegor Kozlovich <k1ppregel@gmail.com>
2015-06-12 19:54:25 +0200

    Initial commit

---
+++ BucketListApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BucketListApp.MvcApplication" Language="C#" %>
---
+++ BucketListApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BucketListApp.Startup))]
+namespace BucketListApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
